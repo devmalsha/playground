7bc7e560b61d2d14d41ea9f8bc2e7133dec076d7
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-21 15:47:39 +0530

    Check undefined javascript data type

--- js/script.js
+++ js/script.js
@@ -1,14 +1,3 @@
-var message="Global Scope"
-console.log("global: message = "+message);
-
-var a = function (){
-    var message = "inside a"
-    console.log("global: message = "+message);
-    b ();
-}
-
-function b () {
-console.log("global: message = "+message);
-}
-
-a();
+let x;
+x = 5;
+console.log(x);
