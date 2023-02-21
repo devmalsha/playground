3ed3cdb132f219612d09eac7fc3249b0c8f2fe45
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-21 20:27:07 +0530

    Test strict equality

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 let x = 4,
   y = 4;
-if (x == y) {
-  console.log("x & y are equal");
+if (x === y) {
+  console.log("x is equal to y");
 }
 
 x = "4";
-if (x == y) {
-  console.log("x & y are equal");
+if (x === y) {
+  console.log("x is equal to y");
+} else {
+  console.log("x is not equal to y");
 }
