21329b44b92ce4dd691cb0a88041097cd51a2c3c
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-23 19:44:30 +0530

    Test function with object

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-// let company = new Object();
-// company.name = "facebook";
-// console.log(company);
+function callBymyName() {
+  let fullName = new Object();
+  fullName.firstName = "Thedath";
+  fullName.secondName = "Oudarya";
+  console.log(fullName["firstName"] + " " + fullName["secondName"]);
+}
 
-let objectName = new Object();
-objectName.firstProperty = "first value";
-objectName.secondProperty = 2;
-
-console.log(objectName);
-
-objectName.anotherObject = new Object();
-objectName.anotherObject.firstProperty = "first value of another object";
-objectName.anotherObject.secondProperty = 1.2;
-
-console.log(objectName["anotherObject"]);
-console.log(objectName.anotherObject);
-console.log(
-  "Property/value of another object: " + objectName.anotherObject.firstProperty
-);
-objectName["thirdProperty"] = "3rd Value";
-console.log(
-  "Third property of the Parent object is: " + objectName["thirdProperty"]
-);
+callBymyName();
