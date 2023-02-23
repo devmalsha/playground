79be8d6d62cacadefa1ca77c690a4e4e0598c432
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-23 19:27:29 +0530

    Test object creating methods and calling methods

--- js/script.js
+++ js/script.js
@@ -7,3 +7,17 @@ objectName.firstProperty = "first value";
 objectName.secondProperty = 2;
 
 console.log(objectName);
+
+objectName.anotherObject = new Object();
+objectName.anotherObject.firstProperty = "first value of another object";
+objectName.anotherObject.secondProperty = 1.2;
+
+console.log(objectName["anotherObject"]);
+console.log(objectName.anotherObject);
+console.log(
+  "Property/value of another object: " + objectName.anotherObject.firstProperty
+);
+objectName["thirdProperty"] = "3rd Value";
+console.log(
+  "Third property of the Parent object is: " + objectName["thirdProperty"]
+);
