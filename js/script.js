9358875049a2fb4f2b7d9f744f1e3694df884676
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-24 10:32:03 +0530

    Test function's behavior

--- js/script.js
+++ js/script.js
@@ -1,12 +1,4 @@
-let objectName = {
-  property1: "value 1",
-  property2: "value 2",
-  property3: {
-    subProp1: "subvalue 1",
-    subProp2: "subvalue 2",
-  },
-  property4: "value 4",
-  property5: "value5",
-};
-
-console.log(objectName);
+function multiply(x, y) {
+  return x * y;
+}
+console.log(multiply(5, 3));
