b79756c26f14dc9556bd1d09913d5b7ee94e2fe7
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-24 12:35:27 +0530

    Test function passing as a argument

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
-function multiply(x, y) {
-  return x * y;
+function makeMultiplier(multiplier) {
+  function myFunc(x) {
+    return multiplier * x;
+  }
+  return myFunc;
 }
-console.log(multiply(5, 3));
+
+let multiplyBy3 = makeMultiplier(3);
+console.log(multiplyBy3(10));
+let doubleAll = makeMultiplier(2);
+console.log(doubleAll(100));
+
+// Passing functions as a arguments
+
+function doOperationOn(x, operation) {
+  return operation(x);
+}
+
+let result = doOperationOn(5, multiplyBy3);
+console.log(result);
