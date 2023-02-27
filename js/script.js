0973c98e5188a9dd3a4559d4c281dfcbfafa8d02
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-27 17:07:05 +0530

    Test array short hand creation

--- js/script.js
+++ js/script.js
@@ -8,12 +8,26 @@
 // console.log(array);
 
 // Example 1
-let myArray = new Array();
-myArray[0] = "Malsha";
-myArray[1] = "Gimhani";
-myArray[2] = function (firstName, secName) {
-  console.log("Hello " + firstName + " " + secName + " !");
-};
-myArray[3] = { goal: "Learn web development" };
-console.log(myArray[1]);
-myArray[2](myArray[0], myArray[1]);
+// let myArray = new Array();
+// myArray[0] = "Malsha";
+// myArray[1] = "Gimhani";
+// myArray[2] = function (firstName, secName) {
+//   console.log("Hello " + firstName + " " + secName + " !");
+// };
+// myArray[3] = { goal: "Learn web development" };
+// console.log(myArray[1]); //console logged as "Gimhani"
+// myArray[2](myArray[0], myArray[1]); //console logged as "Hello Malsha Gimhani !"
+// console.log(myArray[3].goal);// console logged as "Learn web development"
+
+// Example 2 - short hand array creation
+
+let myArray2 = ["Malsha", "Gimahani", "Ranasinghe"];
+console.log(myArray2);
+
+let myArray3 = [
+  { firstName: "Malsha" },
+  { secondName: "Gimhani" },
+  "Ranasinghe",
+];
+
+console.log(myArray3);
