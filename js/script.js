2c4285421906f01ff0880f0121e4f3104a23d7f9
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-27 16:46:22 +0530

    Test array index workings

--- js/script.js
+++ js/script.js
@@ -1,8 +1,19 @@
-let array = new Array();
-array[0] = "Malsha";
-array[1] = "1";
-array[2] = function (name) {
-  console.log("Hello");
+// let array = new Array();
+// array[0] = "Malsha";
+// array[1] = "1";
+// array[2] = function (name) {
+//   console.log("Hello");
+// };
+// array[3] = { course: "HTML, CSS & JS" };
+// console.log(array);
+
+// Example 1
+let myArray = new Array();
+myArray[0] = "Malsha";
+myArray[1] = "Gimhani";
+myArray[2] = function (firstName, secName) {
+  console.log("Hello " + firstName + " " + secName + " !");
 };
-array[3] = { course: "HTML, CSS & JS" };
-console.log(array);
+myArray[3] = { goal: "Learn web development" };
+console.log(myArray[1]);
+myArray[2](myArray[0], myArray[1]);
