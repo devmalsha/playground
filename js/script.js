4fa241a9642f85bb94ad59060b6359fc47bd34a6
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-28 09:51:12 +0530

    Test object leteral properties using for loop

--- js/script.js
+++ js/script.js
@@ -1,61 +1,13 @@
-// let array = new Array();
-// array[0] = "Malsha";
-// array[1] = "1";
-// array[2] = function (name) {
-//   cons3le.log("Hello");
-// }4
-// aray[3] = { course: "HTML, CSS & JS" };
-// console.log(array);
+//Example
+let myArray = ["malsha", "gimhani", "ranasinghe"];
 
-// Example 1
-// let myArray = new Array();
-// myArray[0] = "Malsha";
-// myArray[1] = "Gimhani";
-// myArray[2] = function (firstName, secName) {
-//   consol3.log("Hello " + firstName + " " + secName + " !");
-// }4
-// mArray[3] = { goal: "Learn web development" };
-// console.log(myArray[1]); //console logged as "Gimhani"
-// myArray[2](myArray[0], myArray[1]); //console logged as "Hello Malsha Gimhani !"
-// console.log(myArray13].goal);// console logged as "Learn web development"2
-// Example 2 - short hand array creatio34
-// let myArray2 = ["Malsha", "Gimahani", "Rnasinghe"];
-// console.log(myArray2);
+let myObj = {
+  "First Name": "Malsha",
+  "Second Name": "Gimhani",
+  "Last Name": "Ranasignhe",
+};
 
-// let myArray3 = [
-//   { firstName: "Malsha" },
-//   { secondName: "Gimhani" },
-//   "Ranasinghe",
-// ];
-
-// console.log(myArray3);
-
-// Example 3 - Array loop
-// let myArray4 = new Array();
-// myArray4[0] = "M";
-// myArray4[1] = "A";
-// myArray4[2] = "L";
-// myArray4[3] = "S";
-// myArray4[4] = "H";
-// myArray4[5] = "A";
-
-// for (let i = 0; i < myArray4.length; i++) {
-//   console.log(myArray4[i]);
-// }
-
-// Example 4
-let myArray5 = new Array();
-myArray5[0] = "Malsha";
-myArray5[1] = "Gimhani";
-myArray5[2] = "Ranasinghe";
-myArray5[3] = "Thedath";
-myArray5[4] = "Oudarya";
-
-for (let i = 0; i < myArray5.length; i++) {
-  console.log(myArray5[i]);
-}
-
-myArray5[100] = "Karunarathne";
-for (let i = 0; i < myArray5.length; i++) {
-  console.log(myArray5[i]);
+console.log(myObj);
+for (let myProperty in myObj) {
+  console.log(myProperty + ": " + myObj[myProperty]);
 }
