74d417ad4567159681c5e3b839e62513fa2d11af
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-28 20:43:15 +0530

    Test example on IIFE

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,8 @@
-let malshaGreeter = {};
-malshaGreeter.myName = "Malsha";
-malshaGreeter.sayHello = function () {
-  console.log("Hello " + malshaGreeter.myName);
-};
+(function (window) {
+  let malshaGreeter = {};
+  malshaGreeter.myName = "Malsha";
+  malshaGreeter.sayHello = function () {
+    console.log("Hello " + malshaGreeter.myName);
+  };
+  window.malshaGreeter = malshaGreeter;
+})(window);
