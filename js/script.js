6cc16a10e6dbbfae8df56a6012a8e5687157e2b3
Malsha Gimhani <dev.malsha93@gmail.com>
2023-02-28 10:36:55 +0530

    Test object behevior of array

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 //Example
 let myArray = ["malsha", "gimhani", "ranasinghe"];
 
-let myObj = {
-  "First Name": "Malsha",
-  "Second Name": "Gimhani",
-  "Last Name": "Ranasignhe",
-};
+// let myObj = {
+//   "First Name": "Malsha",
+//   "Second Name": "Gimhani",
+//   "Last Name": "Ranasignhe",
+// };
 
-console.log(myObj);
-for (let myProperty in myObj) {
-  console.log(myProperty + ": " + myObj[myProperty]);
+// console.log(myObj);
+// for (let myProperty in myObj) {
+//   console.log(myProperty + ": " + myObj[myProperty]);
+// }
+
+// for (let names in myArray) {
+//   console.log("Hello " + myArray[names]);
+// }
+
+myArray.spouse = "Thedath";
+
+for (let names in myArray) {
+  console.log("Hello " + myArray[names]);
 }
