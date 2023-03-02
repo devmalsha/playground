34da670d9e92ee7fb3beb685ff03594c5571337e
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-02 17:56:52 +0530

    Test getElementById

--- js/script1.js
+++ js/script1.js
@@ -1,1 +1,6 @@
-console.log(document.getElementById("tittle"));
+function sayHello() {
+  let name = document.getElementById("name").value;
+  let messege = " <h2> Hello " + name + "! </h2>";
+
+  document.getElementById("content").innerHTML = messege;
+}
