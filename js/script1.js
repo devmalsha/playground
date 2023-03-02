be01ea77e6545f63a73628a0d42e5c889ee40c1d
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-02 20:27:57 +0530

    Test DOMContentLoaded

--- js/script1.js
+++ js/script1.js
@@ -1,14 +1,17 @@
-function sayHello() {
-  let name = document.getElementById("name").value;
-  let messege = " <h2> Hello " + name + "! </h2>";
+document.addEventListener("DOMContentLoaded", function (event) {
+  function sayHello(event) {
+    this.textContent = "Said it!";
+    let name = document.getElementById("name").value;
+    let messege = " <h2> Hello " + name + "! </h2>";
 
-  document.getElementById("content").innerHTML = messege;
+    document.getElementById("content").innerHTML = messege;
 
-  if (name === "student") {
-    let tittle = document.querySelector("h1").textContent;
-    tittle += " & Love it !";
-    document.querySelector("h1").textContent = tittle;
+    if (name === "student") {
+      let tittle = document.querySelector("h1").textContent;
+      tittle += " & Love it !";
+      document.querySelector("h1").textContent = tittle;
+    }
   }
-}
 
-document.querySelector("button").addEventListener("click", sayHello);
+  document.querySelector("button").addEventListener("click", sayHello);
+});
