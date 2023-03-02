faafcc4d20f2ccb279899c65e63293ed764b2a93
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-02 19:16:46 +0530

    Test querySelector

--- js/script1.js
+++ js/script1.js
@@ -3,4 +3,9 @@ function sayHello() {
   let messege = " <h2> Hello " + name + "! </h2>";
 
   document.getElementById("content").innerHTML = messege;
+
+  if (name === "student") {
+    let tittle = document.querySelector("#tittle").textContent;
+    tittle += "& Love it !";
+  }
 }
