2a3f4d6793a13197d6f800ef840fb876559db52c
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-04 18:22:37 +0530

    Test server response

--- js/script.js
+++ js/script.js
@@ -1,27 +1,9 @@
 document.addEventListener("DOMContentLoaded", function (event) {
-  function sayHello(event) {
-    console.log(event);
-    this.textContent = "Said it!";
-    let name = document.getElementById("name").value;
-    let messege = " <h2> Hello " + name + "! </h2>";
-
-    document.getElementById("content").innerHTML = messege;
-
-    if (name === "student") {
-      let tittle = document.querySelector("h1").textContent;
-      tittle += " & Love it !";
-      document.querySelector("h1").textContent = tittle;
-    }
-  }
-
-  document.querySelector("button").addEventListener("click", sayHello);
-
-  document
-    .querySelector("body")
-    .addEventListener("mousemove", function (event) {
-      if (event.shiftKey === true) {
-        console.log("X cordinate: " + event.clientX);
-        console.log("Y cordinate: " + event.clientY);
-      }
+  document.querySelector("button").addEventListener("click", function () {
+    $ajaxUtility.sendGetRequest("./data/name.txt", function (request) {
+      let name = request.responseText;
+      document.querySelector("#content").innerHTML =
+        "<h2> Hello " + name + " !";
     });
+  });
 });
