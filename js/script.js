50c4439ae34b2b286191537c9d83e33cc4d50ea0
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-10 19:13:36 +0530

    Exercise 1 - using arrow function

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
-function checkFunc() {
-  let number1 = document.getElementById("value1").value;
-  let number2 = document.getElementById("value2").value;
-  let result = number1 * 1 + number2 * 1;
+// function checkFunc() {
+//   let number1 = document.getElementById("value1").value;
+//   let number2 = document.getElementById("value2").value;
+//   let result = number1 * 1 + number2 * 1;
 
-  if (number1 == "100" || number2 == "100" || result == "100") {
-    let display = "true";
-    document.getElementById("result").innerHTML = display;
-  } else {
-    display = result;
-    document.getElementById("result").innerHTML = display;
-  }
-}
+//   if (number1 == "100" || number2 == "100" || result == "100") {
+//     let display = "true";
+//     document.getElementById("result").innerHTML = display;
+//   } else {
+//     display = result;
+//     document.getElementById("result").innerHTML = display;
+//   }
+// }
+
+let checkFunc = (value1, value2) =>
+  value1 === 100 || value2 === 100 || value1 + value2 === 100;
+console.log(checkFunc(0, 100));
+console.log(checkFunc(100, 0));
+console.log(checkFunc(90, 10));
+console.log(checkFunc(40, 60));
+console.log(checkFunc(40, 0));
