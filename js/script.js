6b2ef8ea862b2fa641c0294dba853af91edb82ed
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-12 14:18:05 +0530

    Create new word using given word-arrow function

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 // Exercise part 2 - Ex -1
-const newString = (word) => {
-  if (word.length <= 3) {
-    return word;
-  } else {
-    let newArrayIs = word.split("");
-    let myArrayFirstPart = newArrayIs.slice(0, 3);
-    let myArraySecondPart = newArrayIs
-      .slice(newArrayIs.length - 3, newArrayIs.length)
-      .reverse();
-    return myArrayFirstPart.join("") + myArraySecondPart.join("");
-  }
-};
-console.log(newString("Tth"));
+// const newString = (word) => {
+//   if (word.length < 3) {
+//     return word;
+//   } else {
+//     let newArrayIs = word.split("");
+//     let myArrayFirstPart = newArrayIs.slice(0, 3);
+//     let myArraySecondPart = newArrayIs.slice(-3).reverse();
+//     return myArrayFirstPart.join("") + myArraySecondPart.join("");
+//   }
+// };
+// console.log(newString("Malsha"));
+
+const makeNewString = (word) =>
+  word.length < 3 ? word : word.slice(0, 3) + word.slice(-3);
+console.log(makeNewString("Malsha"));
+console.log(makeNewString("Ranasinghe"));
+console.log(makeNewString("Gimhani"));
+console.log(makeNewString("Thedath"));
+console.log(makeNewString("ad"));
