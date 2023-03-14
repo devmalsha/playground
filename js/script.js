9b9e77467014086ab5b8e193e63e67f5b1a12fea
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-14 13:16:46 +0530

    Create programe to find the even number count upto some give value

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-// Exercise part 3 - Ex -1
-const findEvenNumbers = function (myArray) {
-  return myArray.filter(findingEvenNumber).length;
-  function findingEvenNumber(items) {
-    return items % 2 == 0;
+// // Exercise part 3 - Ex -1
+// const findEvenNumbers = function (myArray) {
+//   return myArray.filter(findingEvenNumber).length;
+//   function findingEvenNumber(items) {
+//     return items % 2 == 0;
+//   }
+// };
+
+// console.log(findEvenNumbers(["1", "2", "8", "4", "5", "6"]));
+
+// Ex-2
+const numberOfEvenValues = (number) => {
+  let arrayOfEveneNumber = [];
+  for (let i = 0; i <= number; i++) {
+    if (i % 2 == 0) {
+      arrayOfEveneNumber.push(i);
+    }
   }
+  return arrayOfEveneNumber.length;
 };
 
-console.log(findEvenNumbers(["1", "2", "3", "4", "5", "6"]));
+console.log(numberOfEvenValues("50"));
+console.log(numberOfEvenValues("25"));
+console.log(numberOfEvenValues("35"));
+console.log(numberOfEvenValues("21"));
