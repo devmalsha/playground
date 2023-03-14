47d2c550f83cad9b31e3b2bf85534bfa8546ba33
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-14 14:19:11 +0530

    Create programe to find the order of array element is assending or not

--- js/script.js
+++ js/script.js
@@ -9,17 +9,29 @@
 // console.log(findEvenNumbers(["1", "2", "8", "4", "5", "6"]));
 
 // Ex-2
-const numberOfEvenValues = (number) => {
-  let arrayOfEveneNumber = [];
-  for (let i = 0; i <= number; i++) {
-    if (i % 2 == 0) {
-      arrayOfEveneNumber.push(i);
+// const numberOfEvenValues = (number) => {
+//   let arrayOfEveneNumber = [];
+//   for (let i = 0; i <= number; i++) {
+//     if (i % 2 == 0) {
+//       arrayOfEveneNumber.push(i);
+//     }
+//   }
+//   return arrayOfEveneNumber.length;
+// };
+
+// console.log(numberOfEvenValues("50"));
+// console.log(numberOfEvenValues("25"));
+// console.log(numberOfEvenValues("35"));
+// console.log(numberOfEvenValues("21"));
+
+// Ex-3
+const findingArrayOrder = (arrayOfIntergers) => {
+  for (let i = 0; i < arrayOfIntergers.length; i++) {
+    if (arrayOfIntergers[i + 1] < arrayOfIntergers[i]) {
+      return "Not Assending";
     }
+    return "Assending";
   }
-  return arrayOfEveneNumber.length;
 };
 
-console.log(numberOfEvenValues("50"));
-console.log(numberOfEvenValues("25"));
-console.log(numberOfEvenValues("35"));
-console.log(numberOfEvenValues("21"));
+console.log(findingArrayOrder([1, 2, 3, 4]));
