2e9e036ec40d2224aef07be7ccad7975165b3404
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-15 17:46:12 +0530

    Compare propeties of two objects

--- js/script.js
+++ js/script.js
@@ -1,8 +1,15 @@
 // part 4-Ex-1
-const leapYearFinder = (yearIs) =>
-  yearIs % 4 === 0 ? "Leap year" : "Not a leap year";
+// const leapYearFinder = (yearIs) =>
+//   yearIs % 4 === 0 ? "Leap year" : "Not a leap year";
 
-console.log(leapYearFinder(2023));
-console.log(leapYearFinder(2022));
-console.log(leapYearFinder(2021));
-console.log(leapYearFinder(2020));
+// console.log(leapYearFinder(2023));
+// console.log(leapYearFinder(2022));
+// console.log(leapYearFinder(2021));
+// console.log(leapYearFinder(2020));
+
+// Ex-2
+
+const checkEquality = (objectA, objectB) =>
+  Object.keys(objectA).every((prop) => objectB[prop]);
+
+console.log(checkEquality({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3, e: 4 }));
