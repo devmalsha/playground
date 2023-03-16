e4830a3234633c0cfe60220ea9db710aa49d30b5
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-16 22:38:47 +0530

    Create function to sort a string to a alphabatic order-another method

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 // Part - 5 - Ex-1
+// const alphabaticOrder = (str) => {
+//   let arr = str.split("");
+//   return arr.sort().join("");
+// };
+// console.log(alphabaticOrder("malsha"));
+
 const alphabaticOrder = (str) => {
-  let arr = str.split("");
-  return arr.sort().join("");
+  let arr = str
+    .split("")
+    .sort((a, b) => (a > b ? 1 : -1))
+    .join("");
 };
 console.log(alphabaticOrder("malsha"));
