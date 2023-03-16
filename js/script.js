f498233a3edba6b675c6532183e720d8982aa56a
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-16 16:01:26 +0530

    Create hexadecimal code generator

--- js/script.js
+++ js/script.js
@@ -15,8 +15,20 @@
 // console.log(checkEquality({ a: 1, b: 2, c: 3 }, { a: 1, b: 2, c: 3, e: 4 }));
 
 // Ex-3
-const create2DArray = (csvString) =>
-  csvString.split("\n").map((rowArray) => rowArray.split(","));
+// const create2DArray = (csvString) =>
+//   csvString.split("\n").map((rowArray) => rowArray.split(","));
 
-let data = "abc, def, ghi\njkl, mno,pqr\nstu, vux, xya";
-console.log(create2DArray(data));
+// let data = "abc, def, ghi\njkl, mno,pqr\nstu, vux, xya";
+// console.log(create2DArray(data));
+
+const hexaDecimalCodeGen = () => {
+  let hexaCodeArray = ["#"];
+  for (let i = 0; i < 6; i++) {
+    let element1 = Math.floor(Math.random() * 16).toString(16);
+    hexaCodeArray.push(element1);
+  }
+  let word = hexaCodeArray.join("");
+  return word;
+};
+
+console.log(hexaDecimalCodeGen());
