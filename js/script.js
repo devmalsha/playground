4def94df8459c7fc0f3f41252069c040d94a33c9
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-16 23:52:14 +0530

    Create function to count vowels inside of a string

--- js/script.js
+++ js/script.js
@@ -5,10 +5,22 @@
 // };
 // console.log(alphabaticOrder("malsha"));
 
-const alphabaticOrder = (str) => {
-  let arr = str
-    .split("")
-    .sort((a, b) => (a > b ? 1 : -1))
-    .join("");
+// const alphabaticOrder = (str) => {
+//   let arr = str
+//     .split("")
+//     .sort((a, b) => (a > b ? 1 : -1))
+//     .join("");
+// };
+// console.log(alphabaticOrder("malsha"));
+
+// Ex-2
+let vowels = ["a", "e", "i", "o", "u"];
+const countVowels = (str) => {
+  let ele = str.split("");
+  let filterLength = "";
+  for (let i = 0; i < ele.length; i++) {
+    filterLength = ele.filter((x) => vowels.indexOf(x) > -1).length;
+  }
+  return filterLength;
 };
-console.log(alphabaticOrder("malsha"));
+console.log(countVowels("malsha"));
