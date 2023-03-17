e6f4b0f2c16918923a2123431e9d7fa00db709aa
Malsha Gimhani <dev.malsha93@gmail.com>
2023-03-17 12:16:52 +0530

    Create function for coin selection

--- js/script.js
+++ js/script.js
@@ -25,11 +25,33 @@
 // };
 // console.log(countVowels("malsha"));
 
-let vowels = ["a", "e", "i", "o", "u"];
-const countVowels = (str) =>
-  str
-    .split("")
-    .filter((letterNeedToBeFind) => vowels.indexOf(letterNeedToBeFind) > -1)
-    .length;
+// let vowels = ["a", "e", "i", "o", "u"];
+// const countVowels = (str) =>
+//   str
+//     .split("")
+//     .filter((letterNeedToBeFind) => vowels.indexOf(letterNeedToBeFind) > -1)
+//     .length;
 
-console.log(countVowels("javascript"));
+// console.log(countVowels("javascript"));
+
+// Ex-3
+// LK coins - 20, 10, 5, 2, 1
+
+let coinArray = [20, 10, 5, 2, 1];
+
+let coinAmount = "";
+
+let coinSelection = [];
+const coinMaker = (amount) => {
+  for (let i = 0; i < coinArray.length; i++) {
+    coinAmount = Math.floor(amount / coinArray[i]);
+    for (let j = 0; j < coinAmount; j++) {
+      coinSelection.push(coinArray[i]);
+    }
+
+    amount = amount - coinArray[i] * coinAmount;
+  }
+  return coinSelection;
+};
+
+console.log(coinMaker(46));
